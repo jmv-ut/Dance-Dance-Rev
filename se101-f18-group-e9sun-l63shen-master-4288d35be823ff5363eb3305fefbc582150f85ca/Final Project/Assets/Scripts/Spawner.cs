a2a2d6798bee8f11d1252c2bbc641783cec4d6ad
jmv-ut <jmvoss@flash.net>
2022-09-21 21:23:50 -0500

    AdddingSpawner

---
+++ se101-f18-group-e9sun-l63shen-master-4288d35be823ff5363eb3305fefbc582150f85ca/Final Project/Assets/Scripts/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+	public float speed;
+	public float bpm;
+	public GameObject note;
+	private float beatTime;
+	private float beatTimer;
+
+	private void Awake()
+    {
+		//speed is in units per second, so (BPM/60) = BPSec. Place spawner 10*BPs
+		float vertical = (bpm/60)*10;
+        transform.Translate (0f, vertical, 0f);
+    }
+
+	// Use this for initialization
+	void Start () {
+		beatTime = bpm/60;
+		beatTimer = beatTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		targetTime -= Time.deltaTime;
+ 
+		if (beatTimer <= 0.0f)
+		{
+			beatTimer = beatTime;
+			Instantiate(note,transform.position, Quaternion.identity);
+		}		
+	}
+}
